41ccc2d1936e11f771133c5a2e228e8747d268dc
Rob Harding <robbie.harding@hotmail.com>
2023-07-09 16:45:52 -0400

    Fix button

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -91,10 +91,10 @@ const ContactForm: FC<ContactFormProps> = ({}) => {
         <button
           ref={buttonRef}
           onFocus={onFocus}
-          className="border-sky-400 border border-1 py-2 px-4 rounded-lg flex gap-2 select-none duration-200 shadow-custom focus:shadow-customFocus -translate-x-1 -translate-y-1 focus:translate-x-0 focus:translate-y-0 bg-slate-50 hover:bg-slate-100"
+          className="border-sky-400 font-semibold border border-1 py-2 px-4 rounded-lg flex gap-2 select-none duration-200 shadow-custom focus:shadow-customFocus -translate-x-1 -translate-y-1 focus:translate-x-0 focus:translate-y-0 bg-slate-50 hover:bg-slate-100"
         >
-          <Mail className="h-4 w-4 m-auto" />
-          Newsletter
+          <Send className="h-4 w-4 m-auto" />
+          Send
         </button>
       </form>
     </Form>
