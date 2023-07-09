8033c4fdd11cbac507d520b526465b61fa4fd3bc
Rob Harding <robbie.harding@hotmail.com>
2023-07-09 17:09:05 -0400

    Fix button mobile

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -90,10 +90,10 @@ const ContactForm: FC<ContactFormProps> = ({}) => {
           )}
         />
         <button
-          onFocus={onClick}
+          onClick={onClick}
           className={
             cn(
-              "cursor-pointer font-semibold py-2 px-6 rounded-lg flex gap-2 select-none duration-200 shadow-custom -translate-x-1 -translate-y-1 bg-slate-50 hover:bg-white",
+              "font-semibold py-2 px-6 rounded-lg flex gap-2 select-none duration-200 shadow-custom -translate-x-1 -translate-y-1 bg-slate-50 hover:bg-white",
               buttonDown && "translate-x-0 translate-y-0 shadow-customDown"
             ) + " border-slate-900 border border-1"
           }
