142439fbe27e46dd57a840b844056dff930f079b
Rob Harding <robbie.harding@hotmail.com>
2023-07-09 17:06:34 -0400

    Remove shadcn-ui button

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,7 +5,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
